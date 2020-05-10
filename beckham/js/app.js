39cc341c572fceb14f938c620a1d22244524c594
Pratik Singh <56778773+kitarp29@users.noreply.github.com>
2020-05-10 21:49:05 +0530

    v16

--- beckham/js/app.js
+++ beckham/js/app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('./sw.js')
+  navigator.serviceWorker.register('/sw.js')
   .then((reg) => console.log('registered!',reg))
   .catch((err) =>  console.log('not registered',err));
 }
