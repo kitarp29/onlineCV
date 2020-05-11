c1e416954e036b3cb03c7c3722d2ef186ba6cee3
Pratik Singh <56778773+kitarp29@users.noreply.github.com>
2020-05-11 11:29:15 +0530

    v30

--- beckham/js/app.js
+++ beckham/js/app.js
@@ -3,3 +3,14 @@ if('serviceWorker' in navigator){
   .then((reg) => console.log('registered!',reg))
   .catch((err) =>  console.log('not registered',err));
 }
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+});
